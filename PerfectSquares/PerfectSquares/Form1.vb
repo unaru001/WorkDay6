fb28c09cdd0100090393c3749ff0fb07b6692ea1
unaru001 <unaru001@umn.edu>
2013-10-09 15:26:24 -0500

    yay

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -1,9 +1,15 @@
 ﻿Public Class Form1
 
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
+        Dim n As Integer = 1
+        Dim persq As Integer
 
-        lstResult.Items.Clear()
-        lstResult.Items.Add("Not implemented yet")
+        Do While n < 11
+            persq = n ^ 2
+            lstResult.Items.Add("The number is " & persq)
+            n += 1
+        Loop
+
 
     End Sub
 End Class
